4040ea7a86b912c3986de166ae17e3cb00033d58
fertorres05 <fernandotorresserra05@gmail.com>
2024-10-22 13:50:56 +0200

    v1.1

--- app/src/main/java/com/example/saludapp/MainActivity.kt
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.saludapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,30 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+
+        }
+        val btnHello: AppCompatButton =findViewById(R.id.btnHello)
+
+
+
+        btnHello.setOnClickListener {
+            val etName: AppCompatEditText = findViewById(R.id.etName)
+
+            val name = etName.text.toString()
+            Log.i("Test", "Botón pulsado")
+            //Toast.makeText(this, "Hola $name!",
+            //Toast.LENGTH_LONG).show()
+            if (name.isNotEmpty()) {
+                Snackbar.make(btnHello, "Hola $name!", Snackbar.LENGTH_SHORT)
+                    .setAnchorView(btnHello)
+                    .setAction("CLOSE") { finishAffinity() }
+                    .show()
+                    etName.setText(null)
+            }
+
+
         }
     }
 }
